19c7e4e0f34bae7e4c78ed6dd7608fe5aa9afc27
avrshay <shaymilo23@gmail.com>
2025-11-22 09:09:49 +0200

    Fixed get_neighbors

--- color_blocks_state.py
+++ color_blocks_state.py
@@ -1,55 +1,80 @@
 import heuristics
 
-#The function accepts a string that represents a final state and returns nothing.
-#המחרוזת שמתקבלת מייצגת רק את הצבעים הגלויים, מחרוזת של מספרים שלמים
-#ההנחה היא שהמחרוזת מגיעה בפורמט של מספרים מופרדים (למשל בפסיקים או רווחים).
+
+# The function accepts a string that represents a final state and returns nothing.
+# המחרוזת שמתקבלת מייצגת רק את הצבעים הגלויים, מחרוזת של מספרים שלמים
+# ההנחה היא שהמחרוזת מגיעה בפורמט של מספרים מופרדים (למשל בפסיקים או רווחים).
+
+
+# helper functions
 def init_goal_for_search(goal_blocks):
 
-    color_blocks_state.final_state = [] #איפוס מבנה קודם כמו שהיה כתוב בהנחיות
+    color_blocks_state.final_state = []  # איפוס מבנה קודם כמו שהיה כתוב בהנחיות
 
-    parts=clean_String_helper(goal_blocks)
+    parts = clean_String_helper(goal_blocks)
 
     for part in parts:
         color_blocks_state.final_state.append(int(part))
 
-
-#helper functions
 def clean_String_helper(str):
     clean_str = str.replace('(', ' ').replace(')', ' ').replace(',', ' ')
     return clean_str.split()  # ['5', '2', '1', '3']
 
+ def convert_list_to_str(block_list):
+        return ",".join([f"({x},{y})" for x, y in block_list])
 
-class color_blocks_state: #מייצג את מצב אפשרי של המגדל
+class color_blocks_state:  # מייצג את מצב אפשרי של המגדל
 
-    final_state=[] #[x, ...., y] #רק את מה שרואים - סטטי כי זה מצב שמשותף לכל המגדלים
+    final_state = []  # [x, ...., y] #רק את מה שרואים - סטטי כי זה מצב שמשותף לכל המגדלים
 
-    #constractor which convert a given string to tuples=state
+    # constractor which convert a given string to tuples=state
     def __init__(self, blocks_str, **kwargs):
-       # you can use the init function for several purposes
-        self.state = []# [(top_cube), (second_cube), ..., (bottom_cube)], (top_cube)=(X,Y)
+        # you can use the init function for several purposes
 
-        color_list=clean_String_helper(blocks_str)
+        self.state = []  # [(top_cube), (second_cube), ..., (bottom_cube)], (top_cube)=(X,Y)
+
+        color_list = clean_String_helper(blocks_str)
 
         for i in range(0, len(color_list), 2):
-            self.state.append((int(color_list[i]), int(color_list[i + 1]))) #covert to int as well
+            self.state.append((int(color_list[i]), int(color_list[i + 1])))  # covert to int as well
 
     @staticmethod
-    #A static function that receives a color_blocks_state object  and
+    # A static function that receives a color_blocks_state object  and
     # returns true if the state is final and false otherwise.
     def is_goal_state(_color_blocks_state):
 
         current_tower = _color_blocks_state.state
-        for block, goal in zip(current_tower, color_blocks_state.final_state): #רץ במקביל על 2 הרשימות
+        for block, goal in zip(current_tower, color_blocks_state.final_state):  # רץ במקביל על 2 הרשימות
             if block[0] != goal:
                 return False
         return True
 
-
-    #The function takes no input and returns a list of tuples such that each one has
+    # The function takes no input and returns a list of tuples such that each one has
     # an object of type color_blocks_state representing one of the neighbors and the
     # cost of moving to that neighbor.
     def get_neighbors(self):
+        # לכל מצב יש 2n-1 שכנים
+        # n סיבובים
+        # n-1 פליפים
 
+        neighbors_list = []
+
+        # new neighbors cause by spin:
+        for i in range(len(self.state)):
+            # spin the block in place i:
+            new_block = (self.state[i][1], self.state[i][0])
+
+            new_neighbor = self.state[:i] + [new_block] + self.state[i + 1:]
+
+            neighbors_list.append(
+                (color_blocks_state(convert_list_to_str(new_neighbor)), 1))  # add the neighbor to the neighbors' list, spin always cost 1
+
+        # new neighbors cause by flip:
+        for i in range(len(self.state) - 1):
+            new_neighbor = self.state[:i] + self.state[i:][::-1]  # flip it
+            neighbors_list.append((color_blocks_state(convert_list_to_str(new_neighbor)), 1))  # flip also cost 1 dua to the forom
+
+        return neighbors_list
 
         # you can change the body of the function if you want
         # def __hash__(self):
@@ -62,4 +87,3 @@ class color_blocks_state: #מייצג את מצב אפשרי של המגדל
     def get_state_str(self):
         return str(self.state)
 
-    #you can add helper functions
